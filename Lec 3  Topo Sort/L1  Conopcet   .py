b5c4ffc7f69dff97b51b8339b45a9f6f3062a3be
Aviral_Sharma_Knp <140230856+aviAVIRAL@users.noreply.github.com>
2025-04-05 16:33:02 +0530

    Add files via upload

---
+++ Lec 3  Topo Sort/L1  Conopcet   .py
@@ -0,0 +1,27 @@
+
+
+
+
+a = [[] for _ in range(5)]
+print(a)
+
+print()
+
+a = [[]*12]
+print(a)
+# a = [[]*V]
+# print(a)     notnwork 
+print()
+print()
+print()
+
+
+
+a = [0]*9
+print(a)
+print()
+
+a = [0 for _ in range(9)]
+print(a)
+
+print()
